4ad0f3e295c9fc187b7cc74aa0f72d8944d3e35e
jingyu.li01 <jingyu.li01@sophgo.com>
2024-03-13 16:36:00 +0800

    Sophgo: Add SPI NOR Flash Driver
     - Add SPI Flash Master Controller driver (SpiDxe) and Nor Flash
       driver (NorFlashDxe).
    
     - Populate SPIFMC resource into the GCD.
    
     - Enable the MMU as early as possible.
    
    Signed-off-by: jingyu.li01 <jingyu.li01@sophgo.com>

--- Silicon/Sophgo/SG2042Pkg/Sec/Platform.c
+++ Silicon/Sophgo/SG2042Pkg/Sec/Platform.c
@@ -2,6 +2,7 @@
 The library call to pass the device tree to DXE via HOB.
 
 Copyright (c) 2021, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
+Copyright (c) 2024, SOPHGO Inc, All rights reserved.<BR>
 
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -156,6 +157,11 @@ PlatformPeimInitialization (
   // Add SDHI resource
   //
   PopulateIoResources (Base, "bitmain,bm-sd");
+
+  //
+  // Add SPI Flash Master Controller resource
+  //
+  PopulateIoResources (Base, "sophgo,spifmc");
 
   return EFI_SUCCESS;
 }

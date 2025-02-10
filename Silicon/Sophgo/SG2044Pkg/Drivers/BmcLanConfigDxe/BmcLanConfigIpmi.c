58021928e7c666ff00d6dacd80693cb30736f912
zhaohui.yan <zhaohui.yan@sophgo.com>
2025-02-10 16:45:43 +0800

    Add BmcLanConfig driver module on SG2044 platform
    Signed-off-by: zhaohui.yan <zhaohui.yan@sophgo.com>

---
+++ Silicon/Sophgo/SG2044Pkg/Drivers/BmcLanConfigDxe/BmcLanConfigIpmi.c
@@ -0,0 +1,471 @@
+/** @file
+  This file defines the interface function required by the BmcLanConfig driver.
+
+  Copyright (c) 2025  Sophgo Corporation. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+#include "BmcLanConfig.h"
+#include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
+#include <Library/IpmiLib.h>
+
+#include <IndustryStandard/Ipmi.h>
+
+EFI_STATUS
+EFIAPI
+IpmiGetBmcLanInfo (
+  IN  UINT8                    BmcChannel,
+  OUT UINT8                    *pAddrSrc,
+  OUT IPMI_LAN_IP_ADDRESS      *BmcIpAddress,
+  OUT IPMI_LAN_SUBNET_MASK     *BmcSubnetMask,
+  OUT IPMI_LAN_DEFAULT_GATEWAY *BmcDefaultGateWay
+  )
+{
+  EFI_STATUS                                      Status;
+  IPMI_GET_CHANNEL_INFO_REQUEST                   GetChannelInfoRequest;
+  IPMI_GET_CHANNEL_INFO_RESPONSE                  GetChannelInfoResponse;
+  IPMI_GET_LAN_CONFIGURATION_PARAMETERS_REQUEST   GetConfigurationParametersRequest;
+  IPMI_GET_LAN_CONFIGURATION_PARAMETERS_RESPONSE  *GetConfigurationParametersResponse;
+  UINT32                                          ResponseSize;
+
+  if ((pAddrSrc == NULL) || (BmcIpAddress == NULL) || (BmcSubnetMask == NULL) || (BmcDefaultGateWay == NULL)) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  //
+  // Get Channel Information
+  //
+  ZeroMem (&GetChannelInfoRequest, sizeof (GetChannelInfoRequest));
+  GetChannelInfoRequest.ChannelNumber.Bits.ChannelNo = BmcChannel;
+  ResponseSize                                       = sizeof (GetChannelInfoResponse);
+
+  Status = IpmiGetChannelInfo (&GetChannelInfoRequest, &GetChannelInfoResponse, &ResponseSize);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to get BMC channel info\n", __func__));
+    return Status;
+  }
+
+  //
+  // Check for LAN interface
+  //
+  if (  EFI_ERROR (Status)
+     || (GetChannelInfoResponse.CompletionCode != IPMI_COMP_CODE_NORMAL)
+     || (GetChannelInfoResponse.MediumType.Bits.ChannelMediumType != IPMI_CHANNEL_MEDIA_TYPE_802_3_LAN))
+  {
+    return EFI_NOT_FOUND;
+  }
+
+  GetConfigurationParametersResponse = AllocateZeroPool (
+                                         sizeof (*GetConfigurationParametersResponse)
+                                         + sizeof (IPMI_LAN_IP_ADDRESS)
+                                         );
+  if (GetConfigurationParametersResponse == NULL) {
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  //
+  // Get LAN Address Source
+  //
+  ZeroMem (&GetConfigurationParametersRequest, sizeof (GetConfigurationParametersRequest));
+  GetConfigurationParametersRequest.ChannelNumber.Uint8 = BmcChannel;
+  GetConfigurationParametersRequest.ParameterSelector   = IpmiLanIpAddressSource;
+  GetConfigurationParametersRequest.SetSelector         = 0;
+  GetConfigurationParametersRequest.BlockSelector       = 0;
+
+  ResponseSize = sizeof (*GetConfigurationParametersResponse) + sizeof (UINT8);
+
+  Status = IpmiGetLanConfigurationParameters (&GetConfigurationParametersRequest, GetConfigurationParametersResponse, &ResponseSize);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to get the LAN ip source\n", __func__));
+    goto Exit;
+  }
+
+  if (GetChannelInfoResponse.CompletionCode != IPMI_COMP_CODE_NORMAL) {
+    Status = EFI_NOT_FOUND;
+    goto Exit;
+  }
+
+  DEBUG ((DEBUG_INFO, "###-ip source = 0x%x\n", GetConfigurationParametersResponse->ParameterData[0]));
+  CopyMem (
+    pAddrSrc,
+    GetConfigurationParametersResponse->ParameterData,
+    sizeof (UINT8)
+    );
+
+  //
+  // Get LAN IP Address
+  //
+  ZeroMem (&GetConfigurationParametersRequest, sizeof (GetConfigurationParametersRequest));
+  GetConfigurationParametersRequest.ChannelNumber.Uint8 = BmcChannel;
+  GetConfigurationParametersRequest.ParameterSelector   = IpmiLanIpAddress;
+  GetConfigurationParametersRequest.SetSelector         = 0;
+  GetConfigurationParametersRequest.BlockSelector       = 0;
+
+  ResponseSize = sizeof (*GetConfigurationParametersResponse) + sizeof (IPMI_LAN_IP_ADDRESS);
+
+  Status = IpmiGetLanConfigurationParameters (&GetConfigurationParametersRequest, GetConfigurationParametersResponse, &ResponseSize);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to get the LAN configuration parameter\n", __func__));
+    goto Exit;
+  }
+
+  if (GetChannelInfoResponse.CompletionCode != IPMI_COMP_CODE_NORMAL) {
+    Status = EFI_NOT_FOUND;
+    goto Exit;
+  }
+
+  CopyMem (
+    BmcIpAddress->IpAddress,
+    GetConfigurationParametersResponse->ParameterData,
+    sizeof (IPMI_LAN_IP_ADDRESS)
+    );
+
+  //
+  // Get Subnet Mask
+  //
+  ZeroMem (&GetConfigurationParametersRequest, sizeof (GetConfigurationParametersRequest));
+  GetConfigurationParametersRequest.ChannelNumber.Uint8 = BmcChannel;
+  GetConfigurationParametersRequest.ParameterSelector   = IpmiLanSubnetMask;
+  GetConfigurationParametersRequest.SetSelector         = 0;
+  GetConfigurationParametersRequest.BlockSelector       = 0;
+
+  ResponseSize = sizeof (*GetConfigurationParametersResponse) + sizeof (IPMI_LAN_SUBNET_MASK);
+
+  Status = IpmiGetLanConfigurationParameters (&GetConfigurationParametersRequest, GetConfigurationParametersResponse, &ResponseSize);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to get the LAN configuration parameter\n", __func__));
+    goto Exit;
+  }
+
+  if (GetChannelInfoResponse.CompletionCode != IPMI_COMP_CODE_NORMAL) {
+    Status = EFI_NOT_FOUND;
+    goto Exit;
+  }
+
+  CopyMem (
+    BmcSubnetMask->IpAddress,
+    GetConfigurationParametersResponse->ParameterData,
+    sizeof (IPMI_LAN_SUBNET_MASK)
+    );
+
+
+  //
+  // Get default gateway
+  //
+  ZeroMem (&GetConfigurationParametersRequest, sizeof (GetConfigurationParametersRequest));
+  GetConfigurationParametersRequest.ChannelNumber.Uint8 = BmcChannel;
+  GetConfigurationParametersRequest.ParameterSelector   = IpmiLanDefaultGateway;
+  GetConfigurationParametersRequest.SetSelector         = 0;
+  GetConfigurationParametersRequest.BlockSelector       = 0;
+
+  ResponseSize = sizeof (*GetConfigurationParametersResponse) + sizeof (IPMI_LAN_SUBNET_MASK);
+
+  Status = IpmiGetLanConfigurationParameters (&GetConfigurationParametersRequest, GetConfigurationParametersResponse, &ResponseSize);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to get the LAN configuration parameter\n", __func__));
+    goto Exit;
+  }
+
+  if (GetChannelInfoResponse.CompletionCode != IPMI_COMP_CODE_NORMAL) {
+    Status = EFI_NOT_FOUND;
+    goto Exit;
+  }
+
+  CopyMem (
+    BmcDefaultGateWay->IpAddress,
+    GetConfigurationParametersResponse->ParameterData,
+    sizeof (IPMI_LAN_SUBNET_MASK)
+    );
+
+Exit:
+  FreePool (GetConfigurationParametersResponse);
+  return Status;
+}
+
+EFI_STATUS
+GetBmcLanInfo (
+  OUT UINT8                 *pAddrSrc,
+  OUT IPMI_LAN_IP_ADDRESS   *BmcIpAddress,
+  OUT IPMI_LAN_SUBNET_MASK  *BmcSubnetMask,
+  OUT IPMI_LAN_DEFAULT_GATEWAY *BmcDefaultGateWay
+  )
+{
+  EFI_STATUS                   Status;
+  UINT8                        BmcChannel;
+
+  for (BmcChannel = IPMI_CHANNEL_NUMBER_IMPLEMENTATION_SPECIFIC_1; BmcChannel < IPMI_CHANNEL_NUMBER_IMPLEMENTATION_SPECIFIC_11; BmcChannel++) {
+    Status = IpmiGetBmcLanInfo (BmcChannel, pAddrSrc, BmcIpAddress, BmcSubnetMask, BmcDefaultGateWay);
+    if (  EFI_ERROR (Status) || (BmcIpAddress->IpAddress[0] == 0)) {
+      continue;
+    }
+    break;
+  }
+  return Status;
+}
+
+EFI_STATUS
+SetIpPram (
+  IN     UINT8          Channel,
+  IN     UINT8          LanParameter,
+  IN OUT UINT8          *IpAddress
+  )
+{
+  UINT8       Commanddata[20];
+  UINT8       Commanddatasize;
+  UINT8       Response[10];
+  UINT32       Responsesize;
+  EFI_STATUS  Status;
+
+
+  Commanddata[0]  = Channel;
+  Commanddata[1]  = LanParameter;
+  CopyMem (&Commanddata[2], &IpAddress[0], 4);
+  Commanddatasize = 6;
+  Responsesize = 10;
+
+  Status = IpmiSubmitCommand (
+            IPMI_NETFN_TRANSPORT,
+            IPMI_TRANSPORT_SET_LAN_CONFIG_PARAMETERS,
+            (UINT8 *) &Commanddata[0],
+            Commanddatasize,
+            (UINT8 *) &Response,
+            (UINT32 *) &Responsesize
+            );
+
+  return Status;
+}
+
+EFI_STATUS
+IpmiSetBmcLanIpSrc (
+  IN UINT8                     BmcChannel,
+  IN UINT8                     *pAddrSrc
+  )
+{
+  UINT8       Commanddata[20];
+  UINT8       Commanddatasize;
+  UINT8       Response[20];
+  UINT32       Responsesize;
+  EFI_STATUS  Status;
+
+  ZeroMem (Commanddata, 20);
+  ZeroMem (Response, 20);
+  //
+  // Set IP Source
+  //
+  Commanddata[0] = BmcChannel;               // Channel number
+  Commanddata[1] = IpmiLanIpAddressSource;        // Parameter selector
+  Commanddata[2] = *pAddrSrc;  // IP Source (STATIC/ DYANAMIC)
+  Commanddatasize = 3;
+  Responsesize    = 10;
+
+  Status = IpmiSubmitCommand (
+            IPMI_NETFN_TRANSPORT,           // NetFunction
+            IPMI_TRANSPORT_SET_LAN_CONFIG_PARAMETERS,     // Command
+            (UINT8 *) &Commanddata[0],  // *CommandData
+            Commanddatasize,            // CommandDataSize
+            (UINT8 *) &Response,        // *ResponseData
+            (UINT32 *) &Responsesize     // *ResponseDataSize
+            );
+  return Status;
+}
+
+EFI_STATUS
+IpmiSetBmcLanIpAddr (
+  IN UINT8                     BmcChannel,
+  IN IPMI_LAN_IP_ADDRESS       *BmcIpAddress
+  )
+{
+  EFI_STATUS  Status;
+
+  Status = SetIpPram (BmcChannel, IpmiLanIpAddress, &BmcIpAddress->IpAddress[0]);
+
+  return Status;
+}
+
+EFI_STATUS
+IpmiSetBmcLanSubnetMask (
+  IN UINT8                     BmcChannel,
+  IN IPMI_LAN_SUBNET_MASK      *BmcSubnetMask
+  )
+{
+  EFI_STATUS  Status;
+
+  Status = SetIpPram (BmcChannel, IpmiLanSubnetMask, &BmcSubnetMask->IpAddress[0]);
+
+  return Status;
+}
+
+EFI_STATUS
+IpmiSetBmcLanGateWay (
+  IN UINT8                     BmcChannel,
+  IN IPMI_LAN_DEFAULT_GATEWAY  *BmcDefaultGateWay
+  )
+{
+  EFI_STATUS  Status;
+
+  Status = SetIpPram (BmcChannel, IpmiLanDefaultGateway, &BmcDefaultGateWay->IpAddress[0]);
+
+  return Status;
+}
+
+EFI_STATUS
+IpmiSetBmcLanInfo (
+  IN UINT8                     BmcChannel,
+  IN UINT8                     *pAddrSrc,
+  IN IPMI_LAN_IP_ADDRESS       *BmcIpAddress,
+  IN IPMI_LAN_SUBNET_MASK      *BmcSubnetMask,
+  IN IPMI_LAN_DEFAULT_GATEWAY  *BmcDefaultGateWay
+  )
+{
+  UINT8       Commanddata[20];
+  UINT8       Commanddatasize;
+  UINT8       Response[20];
+  UINT32       Responsesize;
+  EFI_STATUS  Status;
+
+  ZeroMem (Commanddata, 20);
+  ZeroMem (Response, 20);
+  //
+  // Set IP Source
+  //
+  Commanddata[0] = BmcChannel;               // Channel number
+  Commanddata[1] = IpmiLanIpAddressSource;        // Parameter selector
+  Commanddata[2] = *pAddrSrc;  // IP Source (STATIC/ DYANAMIC)
+  Commanddatasize = 3;
+  Responsesize    = 10;
+
+  //
+  //If BMC LAN configuration IP source is not changed, then issue command once.
+  //
+  Status = IpmiSubmitCommand (
+            IPMI_NETFN_TRANSPORT,
+            IPMI_TRANSPORT_SET_LAN_CONFIG_PARAMETERS,
+            (UINT8 *) &Commanddata[0],
+            Commanddatasize,
+            (UINT8 *) &Response,
+            (UINT32 *) &Responsesize
+            );
+
+  //
+  // IP Source is Static.Set Ip Address,Subnet Mask and Gateway Ip
+  //
+  if ((*pAddrSrc) != 2) {
+    Status = SetIpPram (BmcChannel, IpmiLanIpAddress, &BmcIpAddress->IpAddress[0]);
+    if (EFI_ERROR(Status)) {
+      DEBUG((DEBUG_ERROR, "Failed to set bmc ip addr: %r\n", Status));
+      return Status;
+    }
+    Status = SetIpPram (BmcChannel, IpmiLanSubnetMask, &BmcSubnetMask->IpAddress[0]);
+    if (EFI_ERROR(Status)) {
+      DEBUG((DEBUG_ERROR, "Failed to set bmc subnetmask: %r\n", Status));
+      return Status;
+    }
+    Status = SetIpPram (BmcChannel, IpmiLanDefaultGateway, &BmcDefaultGateWay->IpAddress[0]);
+    if (EFI_ERROR(Status)) {
+      DEBUG((DEBUG_ERROR, "Failed to set bmc gateway: %r\n", Status));
+      return Status;
+    }
+  }
+
+  return Status;
+}
+
+EFI_STATUS
+SetBmcLanIpSrc (
+  IN UINT8                    *pAddrSrc
+  )
+{
+  EFI_STATUS                   Status;
+  UINT8                        BmcChannel;
+
+  for (BmcChannel = IPMI_CHANNEL_NUMBER_IMPLEMENTATION_SPECIFIC_1; BmcChannel < IPMI_CHANNEL_NUMBER_IMPLEMENTATION_SPECIFIC_11; BmcChannel++) {
+    Status = IpmiSetBmcLanIpSrc (BmcChannel, pAddrSrc);
+    if ( EFI_ERROR (Status) ) {
+      continue;
+    }
+    break;
+  }
+  return Status;
+
+}
+
+EFI_STATUS
+SetBmcLanIpAddr (
+  IN IPMI_LAN_IP_ADDRESS      *BmcIpAddress
+  )
+{
+  EFI_STATUS                   Status;
+  UINT8                        BmcChannel;
+
+  for (BmcChannel = IPMI_CHANNEL_NUMBER_IMPLEMENTATION_SPECIFIC_1; BmcChannel < IPMI_CHANNEL_NUMBER_IMPLEMENTATION_SPECIFIC_11; BmcChannel++) {
+    Status = IpmiSetBmcLanIpAddr (BmcChannel, BmcIpAddress);
+    if ( EFI_ERROR (Status) ) {
+      continue;
+    }
+    break;
+  }
+  return Status;
+
+}
+
+EFI_STATUS
+SetBmcLanSubnetMask (
+  IN IPMI_LAN_SUBNET_MASK     *BmcSubnetMask
+  )
+{
+  EFI_STATUS                   Status;
+  UINT8                        BmcChannel;
+
+  for (BmcChannel = IPMI_CHANNEL_NUMBER_IMPLEMENTATION_SPECIFIC_1; BmcChannel < IPMI_CHANNEL_NUMBER_IMPLEMENTATION_SPECIFIC_11; BmcChannel++) {
+    Status = IpmiSetBmcLanSubnetMask (BmcChannel, BmcSubnetMask);
+    if ( EFI_ERROR (Status) ) {
+      continue;
+    }
+    break;
+  }
+  return Status;
+
+}
+
+EFI_STATUS
+SetBmcLanGateWay (
+  IN IPMI_LAN_DEFAULT_GATEWAY *BmcDefaultGateWay
+  )
+{
+  EFI_STATUS                   Status;
+  UINT8                        BmcChannel;
+
+  for (BmcChannel = IPMI_CHANNEL_NUMBER_IMPLEMENTATION_SPECIFIC_1; BmcChannel < IPMI_CHANNEL_NUMBER_IMPLEMENTATION_SPECIFIC_11; BmcChannel++) {
+    Status = IpmiSetBmcLanGateWay (BmcChannel, BmcDefaultGateWay);
+    if ( EFI_ERROR (Status) ) {
+      continue;
+    }
+    break;
+  }
+  return Status;
+
+}
+
+EFI_STATUS
+SetBmcLanInfo (
+  IN UINT8                    *pAddrSrc,
+  IN IPMI_LAN_IP_ADDRESS      *BmcIpAddress,
+  IN IPMI_LAN_SUBNET_MASK     *BmcSubnetMask,
+  IN IPMI_LAN_DEFAULT_GATEWAY *BmcDefaultGateWay
+  )
+{
+  EFI_STATUS                   Status;
+  UINT8                        BmcChannel;
+
+  for (BmcChannel = IPMI_CHANNEL_NUMBER_IMPLEMENTATION_SPECIFIC_1; BmcChannel < IPMI_CHANNEL_NUMBER_IMPLEMENTATION_SPECIFIC_11; BmcChannel++) {
+    Status = IpmiSetBmcLanInfo (BmcChannel, pAddrSrc, BmcIpAddress, BmcSubnetMask, BmcDefaultGateWay);
+    if ( EFI_ERROR (Status) ) {
+      continue;
+    }
+    break;
+  }
+  return Status;
+
+}

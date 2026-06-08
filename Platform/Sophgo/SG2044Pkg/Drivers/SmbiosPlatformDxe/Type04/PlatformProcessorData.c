211f8569252fba2751dcd84b0d88b5c64560b2b8
Longbin Li <longbin.li@sophgo.com>
2026-06-08 10:18:36 +0800

    SMBIOS Type04: correct CPU voltage reporting and add efuse logic
    Read CPU serial number from efuse offset 0x3F8 instead of using a
    static string, and update fallback default to "0x00-00-00-00".
    
    Signed-off-by: kun.chang <kun.chang@sophgo.com>
    Signed-off-by: Longbin Li <longbin.li@sophgo.com>

--- Platform/Sophgo/SG2044Pkg/Drivers/SmbiosPlatformDxe/Type04/PlatformProcessorData.c
+++ Platform/Sophgo/SG2044Pkg/Drivers/SmbiosPlatformDxe/Type04/PlatformProcessorData.c
@@ -22,15 +22,15 @@ SMBIOS_PLATFORM_DXE_TABLE_DATA (SMBIOS_TABLE_TYPE4, PlatformProcessor) = {
     2,                                       // manufactuer
     {{0,},{0.}},                             // processor id
     3,                                       // version
-    {0,0,0,0,0,1},                           // voltage
+    {0,0,0,1,0,1},                           // voltage
     0,                                       // external clock
-    2800,                                    // max speed
-    2800,                                    // current speed - requires update
+    2600,                                    // max speed
+    2600,                                    // current speed - requires update
     0x41,                                    // status
     ProcessorUpgradeOther,
-    SMBIOS_HANDLE_L1I,                       // l1 cache handle
-    SMBIOS_HANDLE_L2,                        // l2 cache handle
-    SMBIOS_HANDLE_L3,                        // l3 cache handle
+    0xFFFE,                                  // L1CacheHandle
+    0xFFFE,                                  // L2CacheHandle
+    0xFFFE,                                  // L3CacheHandle
     4,                                       // serial not set
     0,                                       // asset not set
     5,                                       // part number

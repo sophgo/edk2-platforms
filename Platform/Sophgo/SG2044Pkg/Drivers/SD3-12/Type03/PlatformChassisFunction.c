12a0037adc78ef5d2030824acbb202fb38efccff
Longbin Li <longbin.li@sophgo.com>
2026-07-14 17:00:17 +0800

    SG2044/SD3-12,SRM3-70: fix incorrect SMBIOS data
    Set chassis type to Desktop with a single power cord, drop the Type 39
    power supply records, and emit Type 9 system slot records only for
    RC 0/2/5 which have physical slots on this board.
    
    Signed-off-by: Longbin Li <longbin.li@sophgo.com>

--- Platform/Sophgo/SG2044Pkg/Drivers/SD3-12/Type03/PlatformChassisFunction.c
+++ Platform/Sophgo/SG2044Pkg/Drivers/SD3-12/Type03/PlatformChassisFunction.c
@@ -34,8 +34,8 @@ SMBIOS_PLATFORM_DXE_TABLE_FUNCTION (PlatformChassis) {
       return Status;
     }
 
-    InputData->Type = MiscChassisTypeMainServerChassis;
-    InputData->NumberofPowerCords = 2;
+    InputData->Type = MiscChassisTypeDeskTop;
+    InputData->NumberofPowerCords = 1;
 
     HiiSetString (mSmbiosPlatformDxeHiiHandle, InputStrToken->TokenArray[1], L"v1.0", NULL);
 
